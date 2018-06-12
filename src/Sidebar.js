bc1730387ba3c0b35c69bdaa707f2c4a63e510e8
Megan Bennett <mgbenne16@earlham.edu>
2018-06-12 16:13:55 -0400

    Sidebar and Chat Components

---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import React, { Component } fromt 'react'
+
+class Sidebar extends Component {
+    render() {
+        return (
+            <div className='Sidebar'>
+              Sidebar
+            </div>
+        )
+    }
+}
+
+export default Sidebar
