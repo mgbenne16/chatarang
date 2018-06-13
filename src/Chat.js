a9921bd8c5b45f481e47c1dda826cf5ff99e2500
Megan Bennett <mgbenne16@earlham.edu>
2018-06-13 10:44:43 -0400

    Style Chat and ChatHeader

--- src/Chat.js
+++ src/Chat.js
@@ -29,7 +29,7 @@ class Chat extends Component {
 
     render() {
         return (
-            <div className='Chat'>
+            <div className='Chat' style={styles}>
               <ChatHeader />
               <MessageList messages={this.state.messages} />
               <MessageForm addMessage={this.addMessage} />
@@ -38,4 +38,9 @@ class Chat extends Component {
     }
 }
 
+const styles = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+}
 export default Chat 
