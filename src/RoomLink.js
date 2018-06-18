e6c74987196bf32cc808a707766bfd1859d756ce
Megan Bennett <mgbenne16@earlham.edu>
2018-06-18 13:45:04 -0400

    Use NavLink to change rooms

--- src/RoomLink.js
+++ src/RoomLink.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import { StyleSheet, css } from 'aphrodite'
+import { NavLink } from 'react-router-dom'
 
-const RoomLink = ({ room, loadRoom }) => {
-    const handleClick = (ev) => {
-        ev.preventDefault()
-        loadRoom(room)
-    }
-
+const RoomLink = ({ room }) => {
     return (
         <li className={css(styles.item)}>
-          <a 
-            href='/' 
-            className={css(styles.link)}
-            onClick={handleClick}
-          >
+          <NavLink
+            to={`/rooms/${room.name}`}
+            className={css(styles.item)}>
+        > 
             { room.name }
-          </a>
+          </NavLink>
         </li>
     )
 }
